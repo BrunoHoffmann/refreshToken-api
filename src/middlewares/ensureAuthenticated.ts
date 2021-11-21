fe2a43eef8594094d0af8f802d6e03f2d15846c5
Bruno Hoffmann <brunohoffmanndev@gmail.com>
2021-11-21 13:34:18 -0300

    finish middleware

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,3 +1,24 @@
 import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
 
-export function ensureAuthenticated(request, response, next)
+export function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
+  const authToken = request.headers.authorization;
+
+  if (!authToken) {
+    return response.status(401).json({
+      message: 'Token is missing'
+    });
+  }
+
+  const [, token] = authToken.split(' ');
+
+  try {
+    verify(token, '025fa5dd-77ea-40a5-8c1a-ed9dfbd6a1b5');
+
+    return next();
+  } catch (err) {
+    return response.status(401).json({
+      message: 'Token invalid'
+    })
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
 import { AuthenticateUserController } from "./usueCases/authenticateUser/AuthenticateUserController";
 import { CreateUserController } from "./usueCases/createUser/CreateUserController";
 
@@ -10,7 +11,7 @@ const authenticateUserController = new AuthenticateUserController();
 router.post("/users", createUserController.handle);
 router.post('/login', authenticateUserController.handle);
 
-router.get('/courses', (request, response) => {
+router.get('/courses', ensureAuthenticated, (request, response) => {
   return response.json([
     { id: 1, name: 'NodeJS' },
     { id: 2, name: 'ReactJS' },
